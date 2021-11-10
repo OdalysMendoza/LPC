5d7e69e12492ce2b3880fe240745de224d9b31b6
OdalysMendoza <89678219+OdalysMendoza@users.noreply.github.com>
2021-11-10 17:31:18 -0600

    Add files via upload

---
+++ PRACTICA 4/Practica 4.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+#Codificar en base64
+base64 hola_mundo.c
+
+#Decodificar en base64
+base64 -d mystery_img2.txt > mystery_img2.jpg
+
+#Decodificar en base64
+base64 -d mystery_img1.txt > mystery_img2.jpg
